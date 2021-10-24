933ad9a65a98305f42ffc9fec1ecc470644596b1
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-24 23:04:23 +0300

    edit employers list

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,9 +5,9 @@ import './employees-list.css';
 const EmployeesList = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            <EmployeesListItem name="Oleg H." salary={800}/>
+            <EmployeesListItem name="Maria G." salary={2800}/>
+            <EmployeesListItem name="Orest P." salary={180}/>
         </ul>
     )
 }
