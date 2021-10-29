866c7725bcdfff3b43154404ea5ea10f66e70b2d
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-30 00:46:18 +0300

    3

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,6 +14,8 @@ function App() {
         <div className="search-panel">
             <SearchPanel/>
             <AppFilter/>
+            <br/>
+            <AppFilter/>
         </div>
         
         <EmployeesList/>
