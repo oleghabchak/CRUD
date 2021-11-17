057d778142c1ce08a5feb792e350820bf0f40490
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-11-18 00:04:28 +0200

    Update employees-list-item.js
    simple home-work

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -6,8 +6,8 @@ class EmployeesListItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            increase: false
-
+            increase: false ,
+            star: false
         }
     }
 
@@ -16,17 +16,30 @@ class EmployeesListItem extends Component {
             increase: !increase
         }))
     }
+
+    onLike = () => {
+        this.setState(({star}) => ({
+            star: !star
+        }))
+    }
+
     render() {
         const {name, salary} = this.props;
-        const {increase} = this.state;
+        const {increase, star} = this.state;
         let classNames = "list-group-item d-flex justify-content-between";
         if(increase) {
             classNames += " increase" ;
         }
+
+        if(star) {
+            classNames += " like" ;
+        }
     return (
         
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span
+            onClick={this.onLike}
+            className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
