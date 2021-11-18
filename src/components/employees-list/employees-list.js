c051f587dadafba55b390640e6e135c55c4a799d
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-11-18 15:03:28 +0200

    lesson 93
    93

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -7,7 +7,6 @@ const EmployeesList = ({data}) => {
 const elements = data.map(item =>{
     const {id, ...itemProps} = item;
     return (
-        // <EmployeesListItem name={item.name} salary={item.salary}/>,
         <EmployeesListItem key={id} {...itemProps}/>
     )
 })
