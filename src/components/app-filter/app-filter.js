a9bb025ad3e0225194cefbef1ecfadfc222d37eb
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-11-29 01:07:25 +0200

    102

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,8 +1,24 @@
 import "./app-filter.css";
 
 const AppFilter = () => {
+    const buttonsData = [
+        {name:'all' , label:'Всі працівники'},
+        {name:'rise' , label:'На підвищення'},
+        {name:'moreThen1000' , label:'З/П більше 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        return (
+            <button type="button"
+                    className="btn btn-light"
+                    keu={name}>
+                    {label}
+            </button>
+        )
+    })
     return (
         <div className="btn-group">
+            {buttons}
             <button type="button"
                     className="btn btn-light">
                     Всі працівники
