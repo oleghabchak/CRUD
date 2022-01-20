2eb6215344ec2852b3e4e3ba3e0a877f0ceafcc0
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2022-01-20 22:50:00 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,57 +3,60 @@ const app = express();
 const mysql = require('mysql');
 const bodyparser = require('body-parser');
 
-app.use(bodyparser.json)
 
-
-const mysqlConnection = mysql.createConnection({
+const db = mysql.createPool({
     host:"localhost",
     user: "root",
     password: "lao6247K",
     database:"cruddb",
 });
 
-mysqlConnection.connect(err => {
-    if (err){ console.log(err); 
-        return err;
-    }
-    else {
-    console.log('DataBase---OK');
-    }
-       
-});  
+app.get("/", (req, res) => { 
+    const sqlInsert = 
+    " INSERT INTO cruddb.train ( id, city1, city2, departure, arrival) VALUES ('3','Lutsk','IF', '12:45', '20:54');";
+    db.query(sqlInsert, (err, result)=> {
+        res.send("Hello  world!!!");
+        console.log(err)
+    });
+});
 
-let query ='SELECT * FROM cruddb.train'
-    mysqlConnection.query(query,(err, result, field) => {
-        console.log(err);
-        console.log(result);
-        
-        // console.log(field);
-    })
-
-// app.listen(3001,()=> console.log('Express serwer is running at port: 3001'));
-
-// app.get('/train',(res,req) =>{
-//     mysqlConnection.query('SELECT * FROM cruddb.trains',(err, rows, fields)=>{
-//         if(!err)
-//         console.log(rows);
-//         else 
-//         console.log(err);
-//     });
-// });
-
-
-
-
-// app.get( "/", (req, res) => {
+app.listen(3005, () => {
     
-//     const sqlInsert = 
-//     " INSERT INTO trains (City, Route1) VALUES ('1','1');";
-//     db.query(sqlInsert, (err, result)=> {
-//         res.send("Hello  world!!!");
-//     });
+    console.log('running... on port 3005');
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// const express = require('express');
+// const app = express();
+// const mysql = require('mysql');
+// const bodyparser = require('body-parser');
+
+// app.use(bodyparser.json)
+
+
+// const mysqlConnection = mysql.createConnection({
+//     host:"localhost",
+//     user: "root",
+//     password: "lao6247K",
+//     database:"cruddb",
 // });
 
-// app.listen (3005, () => {
-//     console.log('running... on port 3005');
-// });
+// mysqlConnection.connect(err => {
+//     if (err){ console.log(err); 
+//         return err;
+//     }
+//     else {
+//     console.log('DataBase---OK');
+//     }
+       
+// });  
+
+// let query ='SELECT * FROM cruddb.train'
+//     mysqlConnection.query(query,(err, result, field) => {
+//         console.log(err);
+//         console.log(result);
+        
+       
+//     })
+
